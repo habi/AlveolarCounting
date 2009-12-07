263b20c6dac26582f513dc39281b33850620b905
(no author) <(no author)@e912ff89-8011-4f2e-a8c4-b4f02e0045a5>
2009-12-07 16:37:28 +0000

    - removed Sebastiens Code - ported code block for counting points into own function fct_SetPoints.m, which needs an input image and outputs coordinates of positive and negative points - started to work on overlaying function
    git-svn-id: http://code.ana.unibe.ch/svn/AlveolarCounting@2 e912ff89-8011-4f2e-a8c4-b4f02e0045a5

--- AlveolarCounting.m
+++ AlveolarCounting.m
@@ -14,7 +14,7 @@ warning off Images:initSize:adjustingMag
 
 SampleName = 'R108C21A2m_S2';
 slice = 1000;
-interval = 5;
+interval = 1;
 
 Filename1 = [ SampleName sprintf('%04d',slice) '.rec.8bit.tif' ];
 Filename2 = [ SampleName sprintf('%04d',slice+interval) '.rec.8bit.tif' ];
@@ -29,177 +29,48 @@ Img2= imread([ Path Filename2 ]);
 Threshold2 =graythresh(Img2);
 Bitdepth2 = intmax(class(Img2));
 disp([ num2str(Filename2) ' has been Thresholded with ' num2str(Threshold2) ' (' num2str(Bitdepth2*Threshold2) ')' ]);
-
 if isequal(Bitdepth1,Bitdepth2) == 0
     disp(['Img 1 and Img 2 do not have the same bit depth (' class(Bitdepth1) ' vs. ' class(Bitdepth1) '). I am stopping here!'])
 end
 
-img_thresh = im2bw(Img1,Threshold1);
-img_label= bwlabeln(img_thresh);
+Img1_Thresh = im2bw(Img1,Threshold1);
+Img1_Label = bwlabeln(Img1_Thresh);
 
-%img_label(img_label<max(max(img_label)))=0;
+Img2_Thresh = im2bw(Img2,Threshold2);
+Img2_Label = bwlabeln(Img2_Thresh);
 
-% biggest_member =0;
-% for i=1:n
-%     biggest_member(i) = sum(sum(ismember(img_label,i)));
-% end
-% 
-% [area_size,area_index]=max(biggest_member)
-% img_biggestcomp=ismember(img_label,area_index);
+Img3 = im2double(Img2_Label);
+imshow(Img3,[]);
 
-% figure
-%     subplot(121)
-%         imshow(Img1,[])
-% 	subplot(122)
-%         imshow(img_thresh,[])
+DifferenceImage = ((Img1_Thresh - Img2_Thresh) + 1) /2;
 
-scrsz = get(0,'ScreenSize');
-figure('Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
-    imshow(img_label,[])
-    wait=helpdlg('Choose positive Points in shown picture. Stop with right mouse button',...
-        'Selection');
-	uiwait(wait)
-    hold on % let me draw over stuff already drawn
-    xy_pos = []; % empty coordinates at the beginning
-    n = 0; % pointer to zero
-    button = 1; % clicking with left mouse button keeps going, clicking another button ends the selection
-    while button == 1 % abort if another mousebutton than 1 is pressed
-        [xi,yi,button] = ginput(1);
-        if button == 1 % only save point if left mouse button is pressed
-            plot(xi,yi,'g+'); % plot the chosen points as 'r' red '+' crosses over the image
-            n = n+1;
-            xy_pos(:,n) = [xi,yi];
-        end
-    end
-if size(xy_pos,2) >= 1 % If we haven't saved a positive point, we can proceed without showing it to the user
-	figure('Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
-        imshow(Img1);
-        hold on;
-        plot(xy_pos(1,:),xy_pos(2,:),'go')
-        title([ 'You chose these ' num2str(size(xy_pos,2)) ' positive Points'])
-else
-	xy_pos = [];
-	warn=warndlg('You did not choose a positive Point. Proceeding...','Warning');
-    uiwait(warn)
-end
 
-figure('Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
-    imshow(img_label,[])
-    wait=helpdlg('Now choose negative Points in shown picture. Stop with right mouse button',...
-    'Selection');
-    uiwait(wait)
-    hold on % let me draw over stuff already drawn
-    xy = []; % empty coordinates at the beginning
-    n = 0; % pointer to zero
-    button = 1; % clicking with left mouse button keeps going, clicking another button ends the selection
-    while button == 1
-        [xi,yi,button] = ginput(1);
-        if button == 1
-            plot(xi,yi,'r+'); % plot the chosen points as 'r' red '+' crosses
-            n = n+1;
-            xy(:,n) = [xi,yi];
-        end
-    end
-    xy_neg=xy;
-figure('Position',[1 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
-    imshow(Img1);
-    title({[ num2str(size(xy_pos,2)) ' green circles as positive Points'];...
-        [num2str(size(xy_neg,2)) ' red circles as negative Points']})
-    if size(xy,2) >= 1
-        hold on;
-        if isempty(xy_pos)==0 
-            plot(xy_pos(1,:),xy_pos(2,:),'go')
-        end
-        plot(xy_neg(1,:),xy_neg(2,:),'ro')
-    else
-        warn=warndlg('You did not choose a negative Point. Proceeding...',...
-        'Warning');
-        uiwait(warn)
-        if isempty(xy_pos)==0 
-            plot(xy_pos(1,:),xy_pos(2,:),'go')
-        end
-    end   
-
-figure
-	imshow(Img1);
-    title([ num2str(size(xy_pos,2)) ' green circles as positive Points' ...
-        num2str(size(xy_neg,2)) ' red circles as negative Points'])
-    if size(xy_neg,2) >= 1
-        hold on;
-        if isempty(xy_pos)==0 
-            plot(xy_pos(1,:),xy_pos(2,:),'go')
-        end
-        plot(xy_neg(1,:),xy_neg(2,:),'ro')
-    else
-        hold on
-        if isempty(xy_neg)==0 
-            plot(xy_pos(1,:),xy_pos(2,:),'go')
-        else
-            warn=warndlg('You did not choose one point at all. You might start again...',...
-            'Warning');
-        end
-    end   
+%% Overlay Image
+ 
+Color = [ 255 0 0 ] % define overlay colour RGB
+ 
+ImgToOverlay = im2uint8(mat2gray(Img1)); % separate image into 3 channels
+red = ImgToOverlay;
+green = ImgToOverlay;
+blue = ImgToOverlay;
+ 
+red(Img1_Thresh) = Color(1);    % populate channels with thresholded image
+green(Img1_Thresh) = Color(2);
+blue(Img1_Thresh) = Color(3);
+ 
+Overlay = cat(3, red, green, blue); % make RGB-image
+ 
+figure % display everything
+    subplot(131)
+        imshow(Img1,[])
+    subplot(132)
+        imshow(DifferenceImage,[])    
+    subplot(133)    
+        imshow(Overlay,[])
 
 break
-    
-A = Img1(1:size(Img1,1),1:size(Img1,2));
-L = A;
-R1(1:size(L,1),1:size(L,2)) = 1;
-G1(1:size(L,1),1:size(L,2)) = 0;
-B1(1:size(L,1),1:size(L,2)) = 0;
 
-Img2= imread([Path Filename2]);
-Img2= im2bw(Img2,graythresh(Img2));
-C = Img2(1:size(Img2,1),1:size(Img2,2));
-E = C;
-R2(1:size(E,1),1:size(E,2)) = 0;
-G2(1:size(E,1),1:size(E,2)) = 1;
-B2(1:size(E,1),1:size(E,2)) = 1;
 
-if max(max(A)) < 256
-    R1 = uint8(R1);
-    G1 = uint8(G1);
-    B1 = uint8(B1);
-    R2 = uint8(R2);
-    G2 = uint8(G2);
-    B2 = uint8(B2);
-else 
-    R1 = uint16(R1);
-    G1 = uint16(G1);
-    B1 = uint16(B1);
-    R2 = uint16(R2);
-    G2 = uint16(G2);
-    B2 = uint16(B2);
-end
+%% Choose positive and negative Points on image
 
-L(1:size(A,1),1:size(A,2),1) = immultiply(A,R1);
-L(1:size(A,1),1:size(A,2),2) = immultiply(A,G1);
-L(1:size(A,1),1:size(A,2),3) = immultiply(A,B1);
-figure,imshow(L),title('First Img');
-
-E(1:size(C,1),1:size(C,2),1) = immultiply(C,R2);
-E(1:size(C,1),1:size(C,2),2) = immultiply(C,G2);
-E(1:size(C,1),1:size(C,2),3) = immultiply(C,B2);
-figure,imshow(E),title('Second Img');
-
-T = imadd(E,L);
-From=350;
-T = T(From:512+From,From:512+From,:);
-
-figure,imshow(T),title('Superposition 1 & 2');
-line([3,3],[1,692],'LineWidth',2,'Color',[1 0 0]);
-line([size(T,2)-3,size(T,2)-3],[73,768],'LineWidth',2,'Color',[1 0 0]);
-line([3,size(T,2)-3],[73,73],'LineWidth',2,'Color',[1 0 0]);
-line([3,size(T,2)-3],[692,692],'LineWidth',2,'Color',[1 0 0]);
-
-% clear directory_name;
-% directory_name = uigetdir(pathname1) ;
-% directory_name(size(directory_name,2)+1) = '\';
-% InputDialog={'Give the name of the Img:'};
-%             Name='File name';
-%             NumLines=1;
-%             ImgName = inputdlg(InputDialog,Name,NumLines);
-%             ImgName = char(ImgName);
-% directory_name(size(directory_name,2)+1 : size(directory_name,2) + size(ImgName,2)) = ImgName;
-% directory_name(size(directory_name,2)+1 : size(directory_name,2)+4) = '.tif';
-% imwrite(T,directory_name,'Compression','none'); 
+[xy_pos,xy_neg]=fct_SetPoints(image)
